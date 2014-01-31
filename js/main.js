6fa947dce9a47fea6f6d54610cbae7d491beed16
jaredkrinke <jaredkrinke@gmail.com>
2014-01-30 21:02:28 -0800

    Started work on the player and game layer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 ﻿/// <reference path="radius.js" />
 /// <reference path="radius-ui.js" />
 
+function Player() {
+    Entity.call(this);
+    this.width = 15;
+    this.height = 18;
+    this.elements = [new Rectangle(null, null, null, null, 'red')];
+}
+
+Player.prototype = Object.create(Entity.prototype);
+
+function GameLayer() {
+    Layer.call(this);
+    this.player = this.addEntity(new Player());
+}
+
+GameLayer.prototype = Object.create(Layer.prototype);
+
 window.addEventListener('DOMContentLoaded', function () {
     Radius.initialize(document.getElementById('canvas'));
-    Radius.start(new Layer());
+    Radius.start(new GameLayer());
 });
